0ca328fde48316894517c99a8d07df80e22bf558
Tony <lostyu789@163.com>
2016-02-26 18:17:38 +0800

    练习

--- level3/9-定时器管理、函数封装/tool.js
+++ level3/9-定时器管理、函数封装/tool.js
@@ -23,3 +23,43 @@ function doMove(obj, attr, dir, target, endFn) {
         }
     }, 30);
 }
+
+function starMove(obj, json, endFn) {
+    clearInterval(obj.timer);
+
+            obj.timer = setInterval(function() {
+                var bStop = true;
+
+                for(var attr in json){
+                    var cur = 0;
+
+                    if(attr == 'opacity'){
+                        cur = Math.round( parseFloat(getStyle(obj, attr)) * 100 );
+                    }else{
+                        cur = parseInt( getStyle(obj, attr) );
+                    }
+
+                    var speed = (json[attr] - cur)/6;
+                    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);                   
+
+                    if(cur != json[attr]){
+                        bStop = false;
+                    }
+
+                    if(attr == 'opacity'){
+                        obj.style.filter = 'alpha(opacity='+ (cur + speed) +')';
+                        obj.style.opacity = (cur + speed) / 100;
+                    }else{
+                        obj.style[attr] = cur + speed + 'px';
+                    }                   
+                }
+
+                if(bStop){
+                    clearInterval(obj.timer);
+                    if(typeof endFn == 'function'){
+                        endFn();
+                    }
+                }
+                
+            }, 30);
+}
